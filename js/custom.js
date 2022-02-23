e5da3e8f328880c01385b57df97f17c55d4b3c2c
Emran594 <38129881+Emran594@users.noreply.github.com>
2022-02-23 11:27:32 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+/*!
+ * Item: faizu
+ * Description: Personal Portfolio Website
+ * Author/Developer: Emran Sikder
+ * Version: v1.1.0
+ */
+!function(n){"use strict";n((function(){})),n(window).on("load",(function(){}))}
+
+(jQuery);
